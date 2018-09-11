868e111532b97c9824d651585d52c75204ccb3f1
drivas1225 <diego.rivas@ucsp.edu.pe>
2018-09-11 12:19:58 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,236 +1,149 @@
 #define GLUT_DISABLE_ATEXIT_HACK
 #include <windows.h>
-#define KEY_ESC 27
 #include <GL/glut.h>
-#include <set>
-#include <vector>
-#include <map>
-#include <iostream>
-#include <cmath>
-#include <algorithm>
-#include <queue>
-#include <time.h>
+#include <stdlib.h>
+#define KEY_ESC 27
+#include <stdlib.h>
+#include<time.h>
+#include "graph.cpp"
+#include "Point.h"
+#include "nodeAndEdge.h"
+#define REDIM 25
+
+
+CGraph<Point, int> graph;
+
 
 using namespace std;
 
 
-#include "grafo.h"
+void resize(int width, int height)
+{
+    glViewport(0, 0, width, height);
 
-Grafo grafo;
+    glMatrixMode(GL_PROJECTION);//matriz de proyeccion
+    glLoadIdentity();//cargamos la matriz identidad
+    glOrtho(0,600,0,600,-300,300);
+    glMatrixMode(GL_MODELVIEW);//matriz de modelado
+    glLoadIdentity();//matriz identidad
+    glEnable(GL_DEPTH_TEST);//activamos el Test de profundidad
+    glDepthFunc(GL_LEQUAL);//funcion de borrrado buffer depth
 
-struct Camara{
-	bool opcionHeuristica=false;
-	double WIDTH = 850;
-	double HEIGHT = 850;
-	double rotate = 1;
-	Punto CENTER;
-	bool transform=true;
-	Camara(){};
-	Camara(double w,double h){
-		WIDTH=w;
-		HEIGHT=h;
+}
+void nodos()
+{
+    int x;
+    int y;
+    Point* point;
+    glPointSize(10);
+    glBegin(GL_POINTS);
+        for(int i = 0 ; i < graph.getNodesList().size() ;i++){
+            point = &(graph.getNodesList())[i]->m_data;
+            glColor3f(1,1,1);
+            glVertex3f((*point).getX()*REDIM,(*point).getY()*REDIM,0);
+        }
+    glEnd();
+
+}
+
+void edges()
+{
+    int x;
+    int y;
+    int i , j;
+    Point* point;
+    Point* point2;
+    glBegin(GL_LINES);
+        for(i = 0 ; i < graph.getNodesList().size() ;i++){
+            point = &(graph.getNodesList())[i]->m_data;
+            for ( j = 0 ; j <  ((graph.getNodesList())[i]->getEdges().size()) ;j++){
+                point2 = &(graph.getNodesList())[i]->getEdges()[j]->m_node[1]->m_data;
+                glColor3d(255,0,0);
+                glVertex2d((*point).getX()*REDIM,(*point).getY()*REDIM);
+                glVertex2d((*point2).getX()*REDIM,(*point2).getY()*REDIM);
+
+               // cout<< "("<<(*point).getX()*10 << " , "<< (*point).getY()*10<<")"<< "("<<(*point2).getX()*10 << " , "<< (*point2).getY()*10<<")"<<endl;
+            }
+        }
+        //cout<<i <<" "<<j;
+    glEnd();
+
+
+}
+
+void display()
+{
+     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+     glLoadIdentity();
+     //gluLookAt(1.0,1.0,1.0,0.0,0.0,0.0,0.0,1.0,0.0);
+     nodos();
+     edges();
+     glutSwapBuffers();
+}
+
+
+
+void key(unsigned char key, int x, int y)    //funcion para uso del teclado
+{
+    switch(key)
+    {
+          case 27 :
+            exit(0);
+            break;
+        default:
+            break;
+    }
+
+glutPostRedisplay();    //redibuja el display
+}
+
+void idle()
+{
+    glutPostRedisplay();//redibuja el display
+}
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+
+	cout << "GRAPH NODES" << endl;
+	graph.printNodes();
+	cout << "GRAPH EDGES" << endl;
+	graph.printEdges();
+	cout << "Insert Nodes to find path" << endl;
+	int a,b,c,d;
+    //1 double distance = 0f;
+	/*cin >> a >> b >> c >> d;
+	Point A,B;
+	A.setX(a);A.setY(b);
+	B.setX(c);B.setY(d);
+	vector<Point>ans;
+	int distance = graph.pathBetweenNodes(A,B,ans);
+	for(auto i = static_cast<int>(ans.size() - 1); i >= 0; --i ){
+		cout << ans[i].getX() << ", " << ans[i].getY() << " -> ";
 	}
-	double ASPECT(){
-		return WIDTH/HEIGHT;
-	}
-	void zoom(double Scale){
-		rotate*=Scale;
-		transform=true;
-	}
-	void move(double inX,double inY){
-		CENTER+=Punto(inX,inY);
-	}
-};
+	cout << "//" << endl;
+    cout << "COSTO DEL RECORRIDO: " << distance << endl;
+    */
+    glutInit(&argc, argv);//inicializacion del glut
+    glutInitWindowSize(800,600);//tamaño de la ventana
+    glutInitWindowPosition(400,100);//posicion de la ventana en el monitor
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);//modo de visualizacion inicial
+    glutCreateWindow("Malla grafo");//nombre de la ventana
+    glClearColor(0,0,0,0);//especificar valores claros para el color del buffer
+    glutReshapeFunc(resize);//control de ventanas
+    glutDisplayFunc(display);//establece la devolución de llamada de visualización de la ventana actual
+    glutKeyboardFunc(key);//funcion teclado especial
+    //glutFullScreen();
+    glutIdleFunc(idle);//funcion de animacion
 
+   // char soundfile[20] ="C:\planeta.wav";//sonido
+    //PlaySound((LPCSTR)soundfile, NULL, SND_FILENAME | SND_ASYNC );//sonido
 
-Camara CAMARA(850,850);
+    glutMainLoop();//bucle de procesamiento de eventos de GLUT
 
-//Controlar la escena con el teclado
-void keyPress(int key,int x,int y){
-	if(key==KEY_ESC){//salir
-		exit(0);
-	}
-	CAMARA.transform=true;
-	if(key==(int)'+'){//zoom in
-		CAMARA.zoom(2);
-	} else if(key==(int)'-'){//zoom out
-		CAMARA.zoom(.5);
-	} else if(key==GLUT_KEY_RIGHT){//mover derecha
-		CAMARA.move(-15*1/CAMARA.rotate,0);
-	} else if(key==GLUT_KEY_LEFT){//mover izquierda
-		CAMARA.move(15*1/CAMARA.rotate,0);
-	} else if(key==GLUT_KEY_UP){//mover arriba
-		CAMARA.move(0,-15*1/CAMARA.rotate);
-	} else if(key==GLUT_KEY_DOWN){//mover abajo
-		CAMARA.move(0,15*1/CAMARA.rotate);
-	} else if(key==GLUT_KEY_F1){
-		CAMARA.opcionHeuristica=false;
-	} else if(key==GLUT_KEY_F2){
-		CAMARA.opcionHeuristica=true;
-	} else {
-		CAMARA.transform=false;
-	}
-	if(CAMARA.transform)
-        glutPostRedisplay();
-}
-
-//Redimencionar escena de acuerdo a el tamaño de la ventana
-void reshapeScene(GLint width, GLint height){
-	CAMARA.transform=true;
-	if (height < width){
-		double w = (double)height;
-		double left = (width - w) / 2;
-		glViewport(left, 0, w, height);
-	} else {
-		double h=(double)width;
-		double floor = (height-h)/2;
-		glViewport(0,floor,width,h);
-	}
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(-CAMARA.WIDTH/2,CAMARA.WIDTH/2,-CAMARA.HEIGHT/2,CAMARA.HEIGHT/2);
-	glMatrixMode(GL_MODELVIEW);
-
-	glutPostRedisplay();
-}
-
-void graficarArista(Punto A, Punto B,bool obstaculo=false){
-	glVertex2f(A.x+CAMARA.CENTER.x,A.y+CAMARA.CENTER.y);
-	glVertex2f(B.x+CAMARA.CENTER.x,B.y+CAMARA.CENTER.y);
-}
-
-void graficarArista(Nodo* A, Nodo* B,bool obstaculo=false){
-	graficarArista((*A).punto,(*B).punto, obstaculo);
-}
-
-void graficoProfundidad(Nodo* u,set<pair<int,int> >& Obstaculos,set<int>& S,int padreID=-1){
-	S.insert(u->ID);
-	for (auto it=(*u).aristas.begin();it!=(*u).aristas.end();it++){
-		glColor3f(1.0f,1.0f,0);
-		if(S.find(it->first)==S.end()){
-			if(Obstaculos.find(make_pair(u->ID,it->first))!=Obstaculos.end()){
-				glColor3f(1.0f,0.5f,0);
-			}
-			graficarArista(u,it->second);
-			graficoProfundidad(it->second,Obstaculos,S,u->ID);
-		} else if((it->second)->ID!=padreID){
-			if(Obstaculos.find(make_pair(u->ID,it->first))!=Obstaculos.end()){
-				glColor3f(1.0f,0.5f,0);
-			}
-			graficarArista(u,it->second);
-		}
-	}
-}
-
-void graficarAristasDeGrafo(Grafo* G){
-	glBegin(GL_LINES);
-        glColor3f(1.0f,1.0f,0);
-		set<int> S;
-		if(G->size){
-			graficoProfundidad((G->nodos.begin())->second,G->Obstaculos,S);
-		}
-	glEnd();
-}
-
-
-long long xorshift_x, xorshift_y, xorshift_z,xorshift_w;
-
-void SeedXorShift (){
-  srand(time(NULL));
-  time_t timer= time(NULL);
-  struct tm y2k ;
-  y2k.tm_hour = 0;
-  y2k.tm_min = 0;
-  y2k.tm_sec = 0;
-  y2k.tm_year = 100;
-  y2k.tm_mon = 0;
-  y2k.tm_mday = 1;
-  long long seconds= difftime(timer ,mktime(&y2k));//diferencia desde 1970
-  xorshift_x = seconds;
-  xorshift_y = seconds ^ 123456789;//operacion xor
-  xorshift_z=seconds << 31;//shift left
-  xorshift_w=seconds >> 11;//shift rigth
-}
-
-long long XorShift (long long MAX_XOR){
-  long long tmp;
-  tmp=xorshift_x ^( xorshift_x <<11);//xor
-  xorshift_x=xorshift_y ;
-  xorshift_y=xorshift_z ;
-  xorshift_z=xorshift_w ;
-  xorshift_w=xorshift_w ^ ( xorshift_w >>19)^(tmp^(tmp>>8)) ;//xor y shiftrigth
-  return xorshift_w % MAX_XOR;//modulo
-}
-
-
-void generarPuntos(set<Punto>& P,int n){
-	while(P.size()<n){
-		double x=XorShift((long long)CAMARA.WIDTH);
-		double y=XorShift((long long)CAMARA.HEIGHT);
-		P.insert(Punto(x,y));
-	}
-}
-
-void crearGrafo(int n, Grafo* G){
-	set<Punto> P;
-	generarPuntos(P,n);
-	vector<Punto> V;
-	for (auto it=P.begin();it!=P.end();it++){
-		V.push_back(Punto(it->x-CAMARA.WIDTH/2,it->y-CAMARA.HEIGHT/2));
-	}
-	delaunayTriangulation(V,G);
-}
-
-
-
-void renderScene(void){
-	if(CAMARA.transform){ // Si se sufrio una transformación (eg. ajuste de la ventana, movimiento, zoom)
-		gluOrtho2D(-CAMARA.WIDTH/2,CAMARA.WIDTH/2,-CAMARA.HEIGHT/2,CAMARA.HEIGHT/2);
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);//Borrar datos de la escena
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-		glScalef(CAMARA.rotate,CAMARA.rotate,1.0f);//Realizar transformación de escala (zoom_in(x2), zoom_out(x.5))
-		graficarAristasDeGrafo(&grafo);//Graficar la escena con la traslación de camara
-		if(!CAMARA.opcionHeuristica)
-			graficarCaminoDeGrafoProfundidad(&grafo,0,grafo.size-1);
-		else
-			graficarCaminoMejorElPrimero(&grafo,0,grafo.size-1);
-		CAMARA.transform=false;//Se realizo todas las transformaciones
-		glutSwapBuffers();
-	}
-}
-
-void init_GL(void) {
-	//Color del fondo de la escena
-	glClearColor(0.0f, 0.0f, 0.0f, 0.0f); //(R, G, B, transparencia) en este caso un fondo negro
-
-	//modo projeccion
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-}
-
-int main(int argc, char **argv){
-	cout << "Numero de Nodos: ";
-	int n;
-	cin >> n;
-	crearGrafo(n,&grafo);
-
-
-	//Iniciando propiedades de la ventana
-	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH)-CAMARA.WIDTH)/2, (glutGet(GLUT_SCREEN_HEIGHT)-CAMARA.HEIGHT)/2);
-	glutInitWindowSize(CAMARA.WIDTH, CAMARA.HEIGHT);
-	glutCreateWindow("Busqueda");
-	//init_GL();
-
-	//Iniciando funciones de interacción
-	glutDisplayFunc(renderScene);
-	glutSpecialFunc(keyPress);
-	glutReshapeFunc(reshapeScene);
-
-	//start animation
-	glutMainLoop();
-
-	return 0;
+    return 0;
 }
